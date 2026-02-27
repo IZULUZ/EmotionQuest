a296ff1ac0cc27f91fc594c98569bdc044f92079
IZULUZ <dlwm4262@gmail.com>
2026-02-28 00:39:14 +0900

    Create firebase.config.js

---
+++ firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDw773x1yot2uncAMl_gTQ6z6Njhgh1Od8",
+    authDomain: "secret-quest-class.firebaseapp.com",
+    databaseURL: "https://secret-quest-class-default-rtdb.firebaseio.com/",
+    projectId: "secret-quest-class",
+    storageBucket: "secret-quest-class.firebasestorage.app",
+    messagingSenderId: "397245266210",
+    appId: "1:397245266210:web:fde35d6a4dfd6ca7070d7b"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
